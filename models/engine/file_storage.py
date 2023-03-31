0b17c973a963001b6e163ac8295459dcfaffdf1f
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-03-31 20:51:56 +0100

    test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,10 +11,12 @@ class FileStorage:
 
     __file_path = "file_storage.json"
     __objects = {}
+    def __int__(self):
+        self.reload()
 
     def all(self):
         """ Returns the __objects dictionary """
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
         """ sets in __objects the object with the key being
@@ -23,28 +25,34 @@ class FileStorage:
         e.g: BaseModel.a6265f3f-6ed3-43cb-9ded-bf90dc4f44a4
         """
         name = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[name] = obj
+        self.__objects[name] = obj
 
     def save(self):
         """ serializes __objects to the JSON file """
-        # self.reload()
-        save_dict = {}
-        new_dict = FileStorage.__objects.copy()
-        for key, value in new_dict.items():
-            save_dict.update({key: value.to_dict()})
-        with open(FileStorage.__file_path, mode="w") as f:
-            json.dump(save_dict, f)
+        objects_copy = self.__objects.copy()
+        new_dict = {}
+        for key, value in objects_copy.items():
+            if isinstance(value, dict):
+                new_dict[key] = value
+            else:
+                new_dict[key] = value.to_dict()
+        with open(self.__file_path, mode="w") as f:
+            json.dump(new_dict, f)
 
     def reload(self):
         """ deserialized the JSON file to the file path """
         try:
-            with open(FileStorage.__file_path, mode="r") as f:
-                data = f.read()
-            content = json.loads(data)
-            for key, data in content.items():
-                keys = data.get('__class__')
-                if keys in globals():
-                    tmp_instance = globals()[keys](**data)
-                    FileStorage.__objects = tmp_instance
+            with open(self.__file_path, mode="r") as f:
+                data = json.load(f)
+                self.__objects = data
         except FileNotFoundError:
             pass
+
+        # new_dict = {}
+        # for key, value in data.items():
+        #     class_name = data.get("__class__")
+        #     if class_name in globals():
+        #         tmp_instance = globals()[class_name](**value)
+        #         new_dict[key] = tmp_instance
+        # self.__objects = new_dict
+
