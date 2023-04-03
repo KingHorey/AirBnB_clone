dc0c4bb3e8338533e4eecde261d2e27dd116f8d7
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-04-03 12:13:26 +0100

    new case

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,6 +4,7 @@
 
 import unittest
 from models.base_model import BaseModel as base
+from models.engine.file_storage import FileStorage as file
 import datetime
 
 
@@ -27,3 +28,35 @@ class TestBaseModel(unittest.TestCase):
         """ test base_model save's method """
         self.assertEqual(type(self.b.updated_at), datetime.datetime)
 
+
+class TestStorage(unittest.TestCase):
+    """ Testing the storage class """
+    def setUp(self):
+        """ test setup for instantiation"""
+        self.storage = file()
+
+    def test_all(self):
+        self.assertEqual(type(self.storage.all()), dict)
+
+    def test_new(self):
+        self.assertTrue(type(self.storage._FileStorage__objects), dict)
+        # with self.assertRaises(TypeError):
+        #     f.new(self, dict)
+
+    def test_objects(self):
+        self.assertTrue(type(self.storage._FileStorage__objects), dict)
+
+    def test_path(self):
+        with self.assertRaises(FileNotFoundError):
+            with open(self.storage._FileStorage__file_path) as file:
+                f.read()
+
+    def test_save(self):
+        with self.assertRaises(FileNotFoundError):
+            with open("te.txt") as fq:
+                self.storage.save(self)
+
+    def test_reload(self):
+        with self.assertRaises(FileNotFoundError):
+            with open("te.txt") as fq:
+                self.storage.reload(self)
