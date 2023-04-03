26caef502eb65ba9008d30b619709e7edabf736a
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-04-03 05:33:26 +0100

    moved test folder

--- test/test_models/test_storage.py
+++ test/test_models/test_storage.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
 
 """ import unittesting for testing storage and storage file"""
-
-
+import os
 import unittest
-from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage as f
+
 
 class TestStorage(unittest.TestCase):
     """ Testing the storage class """
+
     def test_all(self):
         self.assertEqual(type(f.all(self)), dict)
 
     def test_new(self):
         self.assertTrue(type(f._FileStorage__objects), dict)
+        # with self.assertRaises(TypeError):
+        #     f.new(self, dict)
+
+    def test_objects(self):
+        self.assertTrue(type(f._FileStorage__objects), dict)
+
+    def test_path(self):
+        with self.assertRaises(FileNotFoundError):
+            with open(f._FileStorage__file_path) as file:
+                f.read()
 
     def test_save(self):
         with self.assertRaises(FileNotFoundError):
@@ -26,5 +36,8 @@ class TestStorage(unittest.TestCase):
                 f.reload(self)
 
 
+
+
 if __name__ == "__main__":
     unittest.main()
+
