9335fcd8cbccb55f59f0c87a9039ce5f4c3c5912
KingHorey <oreoluwaakinbo.oa@gmail.com>
2023-04-03 04:04:23 +0100

    moved test folder

--- test/test_models/test_storage.py
+++ test/test_models/test_storage.py
@@ -2,15 +2,10 @@
 
 """ import unittesting for testing storage and storage file"""
 
+
 import unittest
-import os
-import sys
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
-
-from models.engine.file_storage import FileStorage as f
 from models.base_model import BaseModel
-
+from models.engine.file_storage import FileStorage as f
 
 class TestStorage(unittest.TestCase):
     """ Testing the storage class """
@@ -18,12 +13,6 @@ class TestStorage(unittest.TestCase):
         self.assertEqual(type(f.all(self)), dict)
 
     def test_new(self):
-        dict_test = {"created_at": "2023-04-03T02:04:42.855400",
-                     "updated_at": "2023-04-03T02:04:42.855458",
-                     "id": "bd893fc8-5d39-4d91-bda2-7963c5c8ce85",
-                     "name":"My_First_Model", "my_number": 89, "__class__":
-                         BaseModel}
-        r = BaseModel(**dict_test)
         self.assertTrue(type(f._FileStorage__objects), dict)
 
     def test_save(self):
@@ -35,6 +24,7 @@ class TestStorage(unittest.TestCase):
         with self.assertRaises(FileNotFoundError):
             with open("te.txt") as fq:
                 f.reload(self)
+
 
 if __name__ == "__main__":
     unittest.main()
